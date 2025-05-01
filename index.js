56508bb55ba0c8936a5de690e7f2b7cc0240a4c4
PRIMIS1234 <primisakd@gmail.com>
2025-05-01 13:27:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ async function downloadSessionData() {
         return false;
     }
 
-    const sessdata = config.SESSION_ID.split("INCONN~XD")[1];
+    const sessdata = config.SESSION_ID.split("PRIMIS~TECH~V1~")[1];
 
     if (!sessdata || !sessdata.includes("#")) {
         console.error('❌ Invalid SESSION_ID format! It must contain both file ID and decryption key.');
